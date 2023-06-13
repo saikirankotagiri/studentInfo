50fb6e2793dfe6f78287d0be360a5a9a41a46e20
saikiran <saikirankotagirig@gmail.com>
2023-06-13 18:08:19 +0530

    added controller

---
+++ src/main/java/com/info/studentInfo/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.info.studentInfo.controller;
+
+import com.info.studentInfo.entity.Student;
+import com.info.studentInfo.exception.StudentErrorResponse;
+import com.info.studentInfo.exception.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+//    @GetMapping("/students")
+//    public List<Student> getStudents(){
+//
+//    }
+
+//    @GetMapping("/students/{studentId}")
+//    public Student getStudent(@PathVariable int studentId){
+//
+//    }
+
+
+
+}
